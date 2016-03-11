430f3ac921d357ef63546a7383010dd2067f9c49
Carlo Nicolini <carlo.nicolini@iit.it>
2016-03-11 10:22:25 +0100

    ...

--- src/sampling/run_surpr_landscape.m
+++
@@ -1,8 +0,0 @@
-function run_surpr_landscape(n)
-addpath('~/workspace/communityalg');
-A=ring_of_custom_cliques(repmat(5,1,40));
-parfor i=1:n
-[memb,q] = paco_mx(A,'quality',0);
-end
-dlmwrite('../../data/ring_clique_n5_k40.adj',A,'delimiter',' ');
- exit;
